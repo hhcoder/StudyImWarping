1788f84095372a5f5db4e8f50a493f95bc1117d5
hhcoder <hhwu.coder@outlook.com>
2020-04-07 11:20:20 -0700

    add image object

--- core_lib/im_warp.cpp
+++ core_lib/im_warp.cpp
@@ -1,12 +1,54 @@
 #include "nlohmann/json.hpp"
+#include <string>
 #include <vector>
 #include <iostream>
 #include <fstream>
 
-struct img_gray8_t : std::vector<uint8_t>
+template <typename T>
+struct img_generic_t : std::vector<T>
 {
+    static const std::string none("None");
     //allocator
-    //loader
+    img_gray8_t(const int in_width, const int in_height, const int in_stride, const std::string& bin_path = none)
+        : width(in_width), height(in_height), stride(in_stride), std::vector<T>(in_height*in_stride)
+    {
+        if (bin_path != none)
+            read(bin_path)
+    }
+
+    void read(const std::string& bin_path)
+    {
+        std::ofstream ifs(bin_path, std::ios::in | std::ios::binary);
+        if (!ifs)
+        {
+            std::cout << "error open file: " << bin_path;
+        }
+        else
+        {
+            std::size_t buf_size = sizeof(T) * height * stride;
+            ifs.read((char*)&(this->at[0]), buf_size);
+        }
+    }
+
+    void write(const std::string& bin_path)
+    {
+        std::ofstream ofs(bin_path, std::ios::out | std::ios::binary);
+        if (!ofs)
+        {
+            std::cout << "error open file: " << bin_path;
+        }
+        else
+        {
+            std::size_t buf_size = sizeof(T) * height * stride;
+            ofs.write((char*)&(this->at[0]), buf_size);
+        }
+    }
+
+    T* buf() { return &(this->at[0]); }
+
+    int width;
+    int height;
+    int stride;
     //writer
 };
 
@@ -39,32 +81,55 @@ int main(int argc, char* argv[])
         nlohmann::json js_drv;
         ifs_drv >> js_drv;
 
-        std::vector<std::vector<int>> src_ctrl_pts_x = js_drv["src"]["control_points"]["x"];
-        std::vector<std::vector<int>> src_ctrl_pts_y = js_drv["src"]["control_points"]["y"];
-        std::vector<std::vector<int>> dst_ctrl_pts_x = js_drv["dst"]["control_points"]["x"];
-        std::vector<std::vector<int>> dst_ctrl_pts_y = js_drv["dst"]["control_points"]["y"];
-
-        std::cout << "rows: " << src_ctrl_pts_x.size() << std::endl;
-        std::cout << "cols: " << src_ctrl_pts_x[0].size() << std::endl;
-        //for (auto it = src_ctrl_pts_x.begin(); it != src_ctrl_pts_x.end(); it++)
-        //{
-        //    std::vector<int> v = *it;
-        //    std::cout << "(" << v[0] << "," << v[1] << "," << v[2] << "," << v[3] << ")";  
-        //}
-        //std::cout << std::endl;
-        
-        int src_im_width = js_drv["src"]["image_format"]["width"].get<int>();
-        int src_im_height = js_drv["src"]["image_format"]["height"].get<int>();
-
         std::string input_data_fpath = js["dir"].get<std::string>() + js["input_data"].get<std::string>();
 
-        std::cout << "im_width: " << src_im_width << std::endl;
-        std::cout << "im_height: " << src_im_height << std::endl;
         std::ifstream ifs_indata(input_data_fpath.c_str());
         nlohmann::json js_indata;
 
         ifs_indata >> js_indata;
 
+        // Decode the driver setting
+        std::vector<std::vector<int>> src_ctrl_pts_x = js_drv["src"]["control_points"]["x"];
+        std::vector<std::vector<int>> src_ctrl_pts_y = js_drv["src"]["control_points"]["y"];
+        std::vector<std::vector<int>> dst_ctrl_pts_x = js_drv["dst"]["control_points"]["x"];
+        std::vector<std::vector<int>> dst_ctrl_pts_y = js_drv["dst"]["control_points"]["y"];
+
+        const int kp_rows = src_ctrl_pts_x.size();
+        const int kp_cols = src_ctrl_pts_x[0].size();
+
+        int src_im_width = js_drv["src"]["image_format"]["width"].get<int>();
+        int src_im_height = js_drv["src"]["image_format"]["height"].get<int>();
+        int src_im_stride = js_drv["src"]["image_format"]["stride"].get<int>();
+        std::string src_im_format = js_drv["src"]["image_format"]["format"].get<std::string>();
+
+        int dst_im_width = js_drv["dst"]["image_format"]["width"].get<int>();
+        int dst_im_height = js_drv["dst"]["image_format"]["height"].get<int>();
+        int dst_im_stride = js_drv["dst"]["image_format"]["stride"].get<int>();
+        std::string dst_im_format = js_drv["dst"]["image_format"]["format"].get<std::string>();
+
+        std::cout << "src_im_width: " << src_im_width << std::endl;
+        std::cout << "src_im_height: " << src_im_height << std::endl;
+        std::cout << "src_im_stride: " << src_im_stride << std::endl;
+        std::cout << "src_im_format: " << src_im_format << std::endl;
+
+        std::cout << "dst_im_width: " << dst_im_width << std::endl;
+        std::cout << "dst_im_height: " << dst_im_height << std::endl;
+        std::cout << "dst_im_stride: " << dst_im_stride << std::endl;
+        std::cout << "dst_im_format: " << dst_im_format << std::endl;
+
+        std::cout << "kp_rows: " << kp_rows << std::endl;
+        std::cout << "kp_cols: " << kp_cols << std::endl;
+
+        // Decode the input image
+        img_gray8_t
+
+        for (int j = 0; j < kp_rows - 1; j++)
+        {
+            for (int i = 0; i < kp_cols - 1; i++)
+            {
+            }
+        }
+        
     }
     catch (std::exception const& e)
     {
